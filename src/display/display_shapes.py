78d4e636e0ba4e87188927b3af58bdc4a121905b
fdahle <felix.dahle@googlemail.com>
2024-02-20 00:06:40 +0100

    more content

--- src/display/display_shapes.py
+++ src/display/display_shapes.py
@@ -1,3 +1,47 @@
+import copy
+
+import geopandas as gpd
 import matplotlib.pyplot as plt
-def display_shapes(shape_files):
-    pass
+import pyproj
+
+from shapely.ops import transform
+
+def display_shapes(input_shapes, normalize=False, title=None, save_path=None):
+
+    shape_data = copy.deepcopy(input_shapes)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    print(shape_data)
+
+    if normalize:
+        shape_data = _normalize_geometries(shape_data)
+
+    print(shape_data)
+
+
+    # Plot the GeoDataFrame
+    shape_data.plot(ax=ax, alpha=0.5)  # Adjust alpha to change the transparency, makes it easier to visualize overlaps
+
+    plt.axis('equal')  # Set equal scaling by changing axis limits
+    plt.show()
+
+def _normalize_geometries(gdf):
+    """
+    Normalizes the geometries in a GeoDataFrame so that the minimum x and y are 0.
+    """
+    minx, miny, maxx, maxy = gdf.total_bounds
+
+    print(minx, miny, maxx, maxy)
+
+
+    # Apply normalization (translation) to each geometry
+    gdf.geometry = gdf.geometry.apply(lambda geom: _translate_geom(geom, minx, miny))
+    return gdf
+
+def _translate_geom(geom, xoff, yoff):
+    transformer = pyproj.Transformer.from_proj(
+        pyproj.Proj(proj='affine', s11=1, s12=0, s21=0, s22=1, xoff=-xoff, yoff=-yoff),
+        pyproj.Proj(proj='affine', s11=1, s12=0, s21=0, s22=1, xoff=0, yoff=0),
+        always_xy=True)
+    return transform(transformer.transform, geom)
--- src/export/export_geometry.py
+++ src/export/export_geometry.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+
 from typing import Union, Optional, Dict
 
 import geopandas as gpd
@@ -9,38 +11,61 @@ from shapely.wkt import loads
 def export_geometry(geometry: Union[str, shape],
                     output_path: str,
                     attributes: Optional[Dict[str, Union[str, int, float]]] = None,
-                    key_attribute: Optional[str] = None,
+                    key_field: Optional[str] = None,
                     crs: Union[int, str] = 'EPSG:3031',
                     attach: bool = False,
-                    overwrite: bool = False) -> None:
+                    overwrite_file: bool = False,
+                    overwrite_entry: bool = False) -> None:
     """
     Exports a given Shapely geometry (or equivalent WKT string) to a GeoDataFrame and saves it to
-    a file in either SHP or GeoJSON format. The function also allows appending to or
-    overwrite an existing file.
+    a file in either SHP or GeoJSON format. The function allows for appending to, overwriting an
+    entire file, or updating an existing entry in the file.
 
     Args:
         geometry (Union[str, shape]): A Shapely geometry object or its WKT string representation.
         output_path (str): The file path where the geometry should be saved.
         attributes (Optional[Dict[str, Union[str, int, float]]], optional): Attributes to be saved
             alongside the geometry. Defaults to None.
+        key_field (Optional[str], optional): The unique attribute key to identify existing entries
+            for updating. Required if 'overwrite_entry' is True.
         crs (Union[int, str], optional): The coordinate reference system to use.
             Defaults to 'EPSG:3031'.
-        attach (bool, optional): If True, appends the geometry to an existing file. Defaults to False.
-        overwrite (bool, optional): If True, overwrites an existing file. Defaults to False.
-
+        attach (bool, optional): If True, appends the geometry to an existing file.
+            Defaults to False.
+        overwrite_file (bool, optional): If True, overwrites the entire existing file.
+            Defaults to False.
+        overwrite_entry (bool, optional): If True, updates an existing entry based on 'key_field'.
+            Defaults to False.
     Raises:
-        ValueError: If both 'attach' and 'overwrite' are set to True, or if the
-            file extension is unsupported.
+        ValueError: For various conditions such as
+            - conflicting parameters,
+            - unsupported file extensions,
+            - missing 'key_field' when required
     """
 
-    # TODO: Implement attach and overwrite true at the same time to overvrite the image_id
+    # raise error for conflicting overwrite parameters
+    if overwrite_file and overwrite_entry:
+        raise ValueError("'overwrite_file' and 'overwrite_entry' cannot both be true.")
+    if overwrite_file and attach:
+        raise ValueError("'overwrite_file' and 'attach' cannot both be true.")
+    if overwrite_entry and not attach:
+        raise ValueError("'overwrite_entry' requires 'attach' to be True.")
 
-    if attach and overwrite:
-        raise ValueError("'attach' and 'overwrite' cannot be true at the same time.")
+    # raise error for missing key field
+    if overwrite_entry and not key_field:
+        raise ValueError("'key_field' must be specified when 'overwrite_entry' is True.")
 
-    if os.path.isfile(output_path) and not attach and not overwrite:
+    # raise error for missing key field in attributes
+    if overwrite_entry and key_field not in attributes:
+        raise ValueError(f"'key_field' '{key_field}' must be present in attributes when "
+                         f"'overwrite_entry' is used.")
+
+    # raise an error for file conflict
+    if os.path.isfile(output_path) and not overwrite_file and not overwrite_entry and \
+            not attach:
         raise ValueError(f"'{output_path}' already exists.")
 
+    # raise error for file extension
     file_extension = os.path.splitext(output_path)[1].lower()
     if file_extension not in ['.shp', '.geojson']:
         raise ValueError(f"File extension '{file_extension}' is not supported.")
@@ -51,20 +76,49 @@ def export_geometry(geometry: Union[str, shape],
 
     # Prepare the data for GeoDataFrame
     data = {'geometry': [geometry]}
-    if attributes:
+    if attributes is not None:
         for key, value in attributes.items():
+            if isinstance(value, pd.Series):
+                if not value.empty:
+                    value = value.iloc[0]  # Assuming you want the first item if it's a Series
+                else:
+                    raise ValueError(f"The attribute '{key}' is an empty Series.")
             data[key] = [value]
 
     # Create a GeoDataFrame
     gdf = gpd.GeoDataFrame(data, crs=crs)
 
-    # Exporting logic
-    if attach and os.path.isfile(output_path):
-        existing_gdf = gpd.read_file(output_path)
-        gdf = existing_gdf.append(gdf, ignore_index=True)
+    print(gdf)
 
-    if overwrite or not os.path.isfile(output_path) or attach:
-        if file_extension == '.shp':
-            gdf.to_file(output_path)
-        elif file_extension == '.geojson':
-            gdf.to_file(output_path, driver='GeoJSON')
+    # Easy case: no file existing, or we just overwrite file
+    if os.path.isfile(output_path) is False or overwrite_file:
+        gdf.to_file(output_path, driver='GeoJSON' if file_extension == '.geojson' else None)
+        return
+
+    # attach data to the existing file
+    if attach:
+
+        # get existing data
+        existing_gdf = gpd.read_file(output_path)
+
+        # raise error if key field is missing
+        if key_field not in existing_gdf:
+            raise ValueError(f"Key field '{key_field}' not found in the file.")
+
+        # key field value already exists
+        if attributes[key_field] in existing_gdf[key_field].values:
+
+            # remove existing entry if overwrite is True
+            if overwrite_entry:
+                existing_gdf = existing_gdf[existing_gdf[key_field] != attributes[key_field]]
+
+            # raise error because we are not allowed to overwrite
+            else:
+                raise ValueError(f"Key field {key_field} ({existing_gdf[key_field]}) already exists "
+                                 f"in the file. Use 'overwrite_entry' to update.")
+
+        # append the new data
+        gdf = pd.concat([existing_gdf, gdf], ignore_index=True)
+
+        # save to file
+        gdf.to_file(output_path, driver='GeoJSON' if file_extension == '.geojson' else None)
